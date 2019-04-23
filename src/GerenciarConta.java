7904022170783babd70b4773526d194f5d446876
phmiranda <pehhagah.1607@gmail.com>
2019-04-22 21:58:52 -0300

    [add] classe responsável por fazer toda a parte de gerenciamento de contas e métodos bancários

---
+++ src/GerenciarConta.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+
+public class GerenciarConta extends Conta {
+
+    public static void main(String[] args){
+        // comentário
+        Conta conta01 = new Conta();
+        conta01.numero = 301;
+        conta01.depositar(2000);
+        conta01.nome = "Pedro";
+
+        // comentário
+        conta01.sacar(300);
+
+        // comentário
+        conta01.depositar(20);
+
+        // comentário
+        Object[] opcoes = {
+                "Depositar",
+                "Sacar",
+                "Saldo",
+                "Extrato",
+        };
+
+        // comentário
+        int opcao = JOptionPane.showOptionDialog(null,"Escolha uma opção", "Operações Bancárias", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
+
+        // comentário
+        if(opcao == 0){
+            conta01.depositar(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor: ")));
+        }
+
+        // comentário
+        if(opcao ==1 ){
+            String txValor = JOptionPane.showInputDialog("Digite o valor: ");
+            double valor = Double.parseDouble(txValor);
+            boolean sucesso = conta01.sacar(valor);
+            if(sucesso){
+                JOptionPane.showMessageDialog(null,"Saque realizado com sucesso");
+            }else{
+                JOptionPane.showMessageDialog(null,"Saldo insuficiente para saque");
+            }
+        }
+
+        // comentário
+        //if (conta01.sacar(1800)){
+        //    System.out.println("Saque realizado com sucesso");
+        //}else{
+        //    System.out.println("Saldo insuficiente para saque");
+        //}
+        // comentário
+        System.out.println("Nome: "+ conta01.nome + "\nSaldo: " + conta01.getSaldo());
+    }
+}
